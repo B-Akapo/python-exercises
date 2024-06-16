425e64e1be660cfa8bcf9d06a82172f937d9985a
Bunmi Akapo <107144014+B-Akapo@users.noreply.github.com>
2024-06-16 17:40:50 +0100

    Create exe-32.py

---
+++ basic-1/exe-32.py
@@ -0,0 +1,49 @@
+# Description: Write a Python program that computes the greatest common divisor (GCD) of two positive integers.
+# Date: 16th June 2024
+
+import math
+
+
+def main():
+    x_name = "first number"
+    y_name = "second number"
+
+    x, y = get_positive_integer(x_name, y_name)
+    result = find_lcm(x, y)
+    print(f"The LCM of {x} and {y} is: {result}")
+
+
+def get_positive_integer(x_name, y_name):
+    while True:
+        try:
+            x_str = input(f"Please enter a positive integer for {x_name}: ")
+            y_str = input(f"Please enter a positive integer for {y_name}: ")
+
+            # Check if inputs contain only digits
+            if not x_str.isdigit() or not y_str.isdigit():
+                print("Please enter integers only.")
+                continue
+
+            # Convert inputs to integers
+            x = int(x_str)
+            y = int(y_str)
+
+            # Check if inputs are positive
+            if x <= 0 or y <= 0:
+                print("Please enter positive values only.")
+                continue
+
+            # If all checks pass, return x and y
+            return x, y
+
+        except ValueError:
+            print("Please enter integers only.")
+
+
+def find_lcm(num1, num2):
+    lcm_result = math.lcm(num1, num2)
+    return lcm_result
+
+
+if __name__ == "__main__":
+    main()
